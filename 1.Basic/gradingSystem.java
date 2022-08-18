80b8659d81b63ed8857df1bbedb0bdcf79a789b4
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-18 17:43:15 +0530

    Implement ////// in all

--- 1.Basic/gradingSystem.java
+++ 1.Basic/gradingSystem.java
@@ -23,33 +23,33 @@ public class Main {
 
 ///////////////////////////////////////////////////////////////
 
-Grading System
-Easy
+// Grading System
+// Easy
 
-1. You are given as input marks of a student.
-2. Display an appropriate message based on the following rules:
-  	  2.1 for marks above 90, print excellent.
-  	  2.2 for marks above 80 and less than equal to 90, print good.
-  	  2.3 for marks above 70 and less than equal to 80, print fair.
-  	  2.4 for marks above 60 and less than equal to 70, print meets expectations.
-  	  2.5 for marks less than equal to 60, print below par.
+// 1. You are given as input marks of a student.
+// 2. Display an appropriate message based on the following rules:
+//   	  2.1 for marks above 90, print excellent.
+//   	  2.2 for marks above 80 and less than equal to 90, print good.
+//   	  2.3 for marks above 70 and less than equal to 80, print fair.
+//   	  2.4 for marks above 60 and less than equal to 70, print meets expectations.
+//   	  2.5 for marks less than equal to 60, print below par.
 
-Note -> Only change the code in area after - // code here
+// Note -> Only change the code in area after - // code here
 
-Constraints
-No constraints
+// Constraints
+// No constraints
 
-Format
-Input
-Input is handled for you and provided as variable marks
+// Format
+// Input
+// Input is handled for you and provided as variable marks
 
-Output
-Appropriate message as per student's marks
+// Output
+// Appropriate message as per student's marks
 
-Example
-Sample Input
+// Example
+// Sample Input
 
-92
+// 92
 
-Sample Output
-excellent
+// Sample Output
+// excellent
