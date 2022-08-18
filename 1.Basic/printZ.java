0cb52189f9c23da29fa6256437afbf5449c3bc48
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-18 17:33:26 +0530

    prizt Z in second way

--- 1.Basic/printZ.java
+++ 1.Basic/printZ.java
@@ -4,11 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         // Write your code here
-        System.out.println("*****");
-        System.out.println("   *");
-        System.out.println("  *");
-        System.out.println(" *");
-        System.out.println("*****");
+        // 1st way 
+        // System.out.println("*****");
+        // System.out.println("   *");
+        // System.out.println("  *");
+        // System.out.println(" *");
+        // System.out.println("*****");
+
+        // 2nd way
+        System.out.print("*****\n   *\n  *\n *\n*****");
     }
 }
 
