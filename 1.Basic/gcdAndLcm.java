f3dcf9bc611c945003e971f802fb0c345065f451
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-18 22:34:33 +0530

    more optimal coe of GCD and LCM with question

--- 1.Basic/gcdAndLcm.java
+++ 1.Basic/gcdAndLcm.java
@@ -31,3 +31,71 @@
         }
     }
 }
+
+
+// 2- way  more optimal code by using division method OR eulid method
+import java.util.*;
+    
+    public class Main{
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n1 = scn.nextInt() , n2 = scn.nextInt();
+
+        int product = n1 * n2;
+        while(n1%n2 != 0){
+            int rem = n1 % n2;
+            n1 = n2;
+            n2 = rem;
+        }
+
+        int gcd = n2;
+
+        int lcm =( product ) / gcd;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
+// Gcd And Lcm
+// Easy
+
+// 1. You are required to print the Greatest Common Divisor (GCD) of two numbers. 
+
+// 2. You are also required to print the Lowest Common Multiple (LCM) of the same numbers. 
+
+// 3. Take input "num1" and "num2" as the two numbers. 
+
+// 4. Print their GCD and LCM.
+
+
+// Constraints
+// 2 <= n <= 10^9
+
+// Format
+// Input
+// num1
+// num2
+// .. the numbers whose GCD and LCM we have to find.
+
+// Output
+// a
+// b
+// .. where 'a' and 'b' are the GCD and LCM respectively.
+
+// Example
+// Sample Input
+
+// 36
+// 24
+
+// Sample Output
+// 12
+// 72
