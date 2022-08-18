416f232dc0812530ead7819c0e2dcfc9d4c8511d
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-18 22:27:08 +0530

    1st approach of GCD and LCM

---
+++ 1.Basic/gcdAndLcm.java
@@ -0,0 +1,20 @@
+//  GCD                    OR   HCF                     and LCM
+// (Greatest Common Divisor)    (Highest Common factor)
+
+// 1- way 
+a = 24 ; b = 36; gcd;
+min = a;
+if(a > b){
+    min = b;
+}
+// loop ---> min to 2   (24 to 2)
+for(int i = min; i >= 2; i--){
+    if(a%i == 0 && b%i == 0){
+        gcd = i;
+        break;
+    }
+}
+print gcd;
+
+ int lcm = a*b / gcd;
+print lcm;
