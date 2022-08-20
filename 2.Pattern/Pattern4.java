217c42efc244084b60deee70b7a0e98742e265da
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-20 11:09:44 +0530

    added Question

--- 2.Pattern/Pattern4.java
+++ 2.Pattern/Pattern4.java
@@ -27,3 +27,33 @@ public class Main {
 
     }
 }
+
+
+///////////////////////////////////////////////////////////////////
+// Pattern 4
+// Easy
+
+// 1. You are given a number n. 2. You've to create a pattern of * and separated by tab as shown in output format.
+
+
+// Constraints
+// 1 <= n <= 100
+
+// Format
+// Input
+// A number n
+
+// Output
+// pat41
+
+// Example
+// Sample Input
+
+// 5
+
+// Sample Output	
+// 	*	*	*	*	*
+// 		*	*	*	*
+// 			*	*	*
+// 				*   *
+//                  *
