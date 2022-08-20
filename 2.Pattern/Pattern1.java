43a8c37bf5ad23a09af2bbceed3d4d506adf5f38
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-20 09:19:54 +0530

    added pattern1 Ques

--- 2.Pattern/Pattern1.java
+++ 2.Pattern/Pattern1.java
@@ -15,3 +15,32 @@ public class Main {
         }
     }
 }
+
+
+
+////////////////////////////////////////////////////////////
+
+// Pattern 1
+// Easy
+
+// 1. You are given a number n.
+// 2. You've to create a pattern of * and separated by tab as shown in output format.
+
+// Constraints
+// 1 <= n <= 100
+
+// Format
+// Input
+// A number n
+
+// Output
+// pat11
+
+// Example
+// Sample Input
+
+// 2
+
+// Sample Output
+// *	
+// *	*
