1668bd4bd613ca927b47f4450730ef2da7250f05
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-25 11:49:10 +0530

    added Question Reverse an array

--- 3.Array/ReverseArray.java
+++ 3.Array/ReverseArray.java
@@ -38,3 +38,38 @@ public static void main(String[] args) throws Exception {
  }
 
 }
+
+
+
+///////////////////////////////////////////////////////////////////
+
+// Reverse An Array
+// Easy
+
+// 1. You are given a number n, representing the size of array a.
+// 2. You are given n numbers, representing elements of array a.
+// 3. You are required to reverse the contents of array a.
+
+// Constraints
+// 0 <= n < 10^4
+// -10^9 <= a[i] <= 10^9
+
+// Format
+// Input
+// Input is managed for you
+
+// Output
+// Output is managed for you
+
+// Example
+// Sample Input
+
+// 5
+// 1
+// 2
+// 3
+// 4
+// 5
+
+// Sample Output
+// 5 4 3 2 1
