5ea72f165b39a4b94aba327286dd193cacb7735b
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-25 10:55:44 +0530

    span of array

---
+++ 3.Array/SpanofArray.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class SpanofArray{
+
+ public static void main(String[] args) throws Exception { 
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    int arr[] = new int[n];
+    for(int i = 0; i < arr.length; i++){
+        arr[i] = scn.nextInt();
+    }
+
+    int ans = spanOfArray(arr);
+    System.out.println(ans);
+
+ }
+ public static int spanOfArray(int arr[]){
+    //  int min = Integer.MAX_VALUE;
+    //  int max = Integer.MIN_VALUE;
+    int max = arr[0];
+    int min = arr[0];
+     for(int i = 0; i < arr.length; i++){
+         if(arr[i] > max){
+             max = arr[i];
+         }
+         if(arr[i] < min){
+             min = arr[i];
+         }
+        //  System.out.print(min +" "+max);
+     }
+     return max-min;
+ }
+}
+
