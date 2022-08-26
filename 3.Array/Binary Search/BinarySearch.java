6effaf12ff154f2c1c5fbcb9220f6486b6f9d25f
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-26 10:45:01 +0530

    Added Binary Search question

---
+++ 3.Array/Binary Search/BinarySearch.java
@@ -0,0 +1,26 @@
+public class MyClass {
+    public static void main(String args[]) {
+        
+        int nums[] = {10,20,30,40,50,60,70,80,90};
+        int target = 200;
+        int l = 0;
+        int h = nums.length-1;
+        int d = target;
+        
+        while(l <= h){
+            int mid = (l + h)/2;
+            
+            if(d > nums[mid]){
+                l = mid+1;
+            }
+            else if(d < nums[mid]){
+                h = mid-1;
+            }
+            else{
+                System.out.print(mid);
+                return;
+            }
+        }
+        System.out.print("-1");
+   }
+}
