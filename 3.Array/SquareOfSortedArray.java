147a3bb5f581770e3c9d29da21d65884d3731f53
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-27 16:48:23 +0530

    Square of Sorted Array

---
+++ 3.Array/SquareOfSortedArray.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        // int arr[] = new int[nums.length];
+        int temp = 0;    
+        //traverse
+        for(int i = 0; i < nums.length; i++){  
+            nums[i] = nums[i] * nums[i];
+            
+        }
+        // for sorting data in aceending order
+         for (int i = 0; i < nums.length; i++) {     
+            for (int j = i+1; j < nums.length; j++) {     
+               if(nums[i] > nums[j]) {    
+                   temp = nums[i];    
+                   nums[i] = nums[j];    
+                   nums[j] = temp;    
+               }     
+            }     
+        } 
+        
+        return nums;
+    }
+}
+
+
+
+/////////////////////////////////////////////////////////
+// Leetcode
+// 977. Squares of a Sorted Array
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+// Example 2:
+
+// Input: nums = [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums is sorted in non-decreasing order.
+ 
+
+// Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
