a3109e0849146903d6f81e48a1ee76e802a0f55c
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-10 10:39:58 +0530

    next() and nextLine()

---
+++ 4.String/Input.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner scn = new Scanner(System.in);
+        
+        // input -> hamidin dffffdw
+        
+        // String f = scn.next(); // hamidin
+        // String s = scn.next(); // dffffdw
+        // System.out.println(f);
+        // System.out.println(s);
+        
+        String str = scn.nextLine(); // hamidin dffffdw
+        System.out.println(str);
+        
+    }
+}
