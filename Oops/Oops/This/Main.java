7804d43e53af8735f9730754cf970e6454206412
mohdsameerid <mohdsameeredu@gmail.com>
2022-09-30 15:58:24 +0530

    with this keyword

--- Oops/Oops/This/Main.java
+++ Oops/Oops/This/Main.java
@@ -17,4 +17,28 @@ public class Main {
      mc.display();
     }
 }
+// output -> Name: null age: 0
 
+
+
+/////////////////////////////////////////////////////////////
+// with this keyword
+class Demo{
+      int age;
+      String name;
+      Demo(int age, String name){
+          this.age = age;
+          this.name = name;
+      }
+      public void display(){
+          System.out.println("Name: "+name+" age: "+age);
+      }
+}
+
+public class Main {
+    public static void main(String args[]){
+     Demo mc = new Demo(22,"Samir");
+     mc.display();
+    }
+}
+// output -> Name: Samir age: 22
