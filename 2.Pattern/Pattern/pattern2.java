3288eb2b412df77a7a333bd8ada3b4ccb85e9f7d
mohdsameerid <mohdsameeredu@gmail.com>
2022-12-04 14:12:57 +0530

    added one pattern program question

---
+++ 2.Pattern/Pattern/pattern2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if( (i < n/2 && j <= i) || (i > n/2 && j < n-i) || (i == n/2) ) 
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            if((i == n/2-1) || (i == n/2+1))
+             System.out.print(" *");
+            if(i == n/2)
+             System.out.print("***");
+            // System.out.print("1");
+            System.out.print("\n");
+        }
+    }
+}
